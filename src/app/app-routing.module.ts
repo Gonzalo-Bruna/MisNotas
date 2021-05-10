3916ec29b2ffe59a7a35471e36496fc90483eaa6
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-10 18:54:47 -0400

    añadidos routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActualizarNotaComponent } from '../app/componentes/actualizar-nota/actualizar-nota.component';
+import { CrearNotaComponent } from '../app/componentes/crear-nota/crear-nota.component';
+import { VistaNotasComponent } from '../app/componentes/vista-notas/vista-notas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:CrearNotaComponent},
+  {path: "vistaNotas", component:VistaNotasComponent},
+  {path: "actualizarNota", component:ActualizarNotaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
