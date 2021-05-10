f96688b5e676801aa873d6ca276a32d49ff12c01
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-10 19:09:08 -0400

    creado formulariuo

--- src/app/componentes/crear-nota/crear-nota.component.ts
+++ src/app/componentes/crear-nota/crear-nota.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-crear-nota',
@@ -7,7 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearNotaComponent implements OnInit {
 
-  constructor() { }
+  formulario:FormGroup;
+  titulo:AbstractControl;
+  estado:AbstractControl;
+  descripcion:AbstractControl;
+
+  constructor(private fb: FormBuilder) {
+
+    this.formulario = fb.group({
+      titulo:['', [Validators.required]],
+      estado:['', [Validators.required]],
+      descripcion:['', [Validators.required]]
+    });
+
+    this.titulo = this.formulario.get("titulo")?.value;
+    this.estado = this.formulario.get("estado")?.value;
+    this.descripcion = this.formulario.get("descripcion")?.value;
+
+  }
 
   ngOnInit(): void {
   }
