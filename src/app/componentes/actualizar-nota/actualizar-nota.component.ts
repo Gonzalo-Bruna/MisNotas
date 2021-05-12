b10c6c349734cd1de6546bbeced9ac819956110c
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-12 17:05:09 -0400

    añadido state al router link para pasar la nota como dato

--- src/app/componentes/actualizar-nota/actualizar-nota.component.ts
+++ src/app/componentes/actualizar-nota/actualizar-nota.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Nota } from '../../../app/interfaces/nota';
 @Component({
   selector: 'app-actualizar-nota',
   templateUrl: './actualizar-nota.component.html',
   styleUrls: ['./actualizar-nota.component.scss']
 })
 export class ActualizarNotaComponent implements OnInit {
+
+  nota:Nota = history.state.data
+  titulo:string = this.nota.titulo;
+  descripcion:string = this.nota.descripcion;
 
   constructor() { }
 
