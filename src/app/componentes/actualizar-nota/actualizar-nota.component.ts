81360f39b0a4493bf70adafff81c8335eef5ecfe
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-12 18:04:22 -0400

    agregado el form

--- src/app/componentes/actualizar-nota/actualizar-nota.component.ts
+++ src/app/componentes/actualizar-nota/actualizar-nota.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Nota } from '../../../app/interfaces/nota';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Nota, listaNotas} from '../../../app/interfaces/nota';
 @Component({
   selector: 'app-actualizar-nota',
   templateUrl: './actualizar-nota.component.html',
@@ -10,10 +11,39 @@ export class ActualizarNotaComponent implements OnInit {
   nota:Nota = history.state.data;
   titulo:string = this.nota.titulo;
   descripcion:string = this.nota.descripcion;
+  estado:number = this.nota.estado;
 
-  constructor() { }
+  formulario:FormGroup;
+
+  constructor( private fb: FormBuilder ) {
+
+    this.formulario = fb.group({
+      titulo:['', [Validators.required]],
+      estado:['', [Validators.required]],
+      descripcion:['', [Validators.required]]
+    });
+
+  }
 
   ngOnInit(): void {
+
+    this.formulario.get("titulo")?.setValue(this.titulo);
+    this.formulario.get("estado")?.setValue(this.estado);
+    this.formulario.get("descripcion")?.setValue(this.descripcion);
+
+  }
+
+  actualizarNota(){
+
+    for(let i = 0 ; i < listaNotas.length ; i++){
+
+      if(listaNotas[i] === this.nota){
+        console.log("encontrado");
+
+      }
+
+    }
+
   }
 
 }
