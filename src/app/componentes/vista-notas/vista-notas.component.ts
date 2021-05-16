50a1fafbcd8375965a319d443e712a8a5b77c077
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-15 22:44:17 -0400

    cargar datos desde el json cada vez que se cargue la pagina de vista notas

--- src/app/componentes/vista-notas/vista-notas.component.ts
+++ src/app/componentes/vista-notas/vista-notas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { listaNotas, Nota, notaActualizar } from '../../../app/interfaces/nota';
+import { ServicioNotasService } from '../../../app/servicios/servicio-notas.service';
 
 @Component({
   selector: 'app-vista-notas',
@@ -10,22 +11,34 @@ export class VistaNotasComponent implements OnInit {
 
   notas:Array<Nota> = listaNotas;
 
-  constructor() { }
+  constructor(private servicio:ServicioNotasService) { }
 
   ngOnInit(): void {
+
+    this.servicio.recibirDatos().subscribe( datos => {
+      //console.log(datos);
+      for(let i = 0 ; i < datos.length ; i++){
+        listaNotas[i] = datos[i];
+      }
+    });
+    
   }
 
   eliminarNota(nota:Nota){
     for(let i = 0 ; i < this.notas.length ; i++){
       if(nota == this.notas[i]){
         this.notas.splice(i, 1);
+        //console.log(this.notas);
+        this.servicio.guardarDatos(this.notas).subscribe(datos => {
+          console.log(datos);
+        });
         return;
       } 
     }
   }
 
   actualizarNota(nota:Nota){
-    notaActualizar.push(nota);
+    notaActualizar[0] = nota;
   }
 
 }
