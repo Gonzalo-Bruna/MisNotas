6c56eb844ccee77f392015481679eb878f174e77
Gonzalo-Bruna <gonzalo.antoniobc@gmail.com>
2021-05-15 22:42:55 -0400

    añadidas metodos para guardar y recibir datos

--- src/app/servicios/servicio-notas.service.ts
+++ src/app/servicios/servicio-notas.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Observable } from 'rxjs';
+import { Nota } from '../../app/interfaces/nota';
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioNotasService {
 
-  url:string = "http://localhost/misNotas/backend";
+  url:string = "http://localhost/misNotas/backend/";
 
   constructor(private servicio:HttpClient) { }
+
+  guardarDatos(listaNotas:Array<Nota>):Observable<any>{
+    return this.servicio.post(`${this.url}guardarDatos.php`, JSON.stringify(listaNotas));
+  }
+
+  recibirDatos():Observable<any>{
+    return this.servicio.get(`${this.url}recibirDatos.php`);
+  }
+
 }
